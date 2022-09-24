c7ce47abcf4b066cbd300e4e6ee90a73287e1dbb
Senbai Kang <senbaikang@gmail.com>
2022-09-24 18:52:50 +0200

    feat: Snakefile is now the default

--- run/run.sh
+++ run/run.sh
@@ -8,14 +8,14 @@
 ##################################
 
 # conda environment name
-ENV_NAME="snakemake"
+ENV_NAME="snake"
 
 # whether to use taskset (1) or not (0)
 USETASKSET=1
 
 # if use taskset, set starting and ending cpu indices, both included
 STARTCPU=0
-ENDCPU=10
+ENDCPU=7
 
 # if not use taskset, set the number of cores you want to use
 CORENUM=1
@@ -34,24 +34,32 @@ LOGFILE="./out"
 #           DO NOT EDIT           #
 ###################################
 
+if [[ -z $1 ]]; then
+  echo "> Unset snakemake file; use \"Snakefile\" by default." >>"${LOGFILE}"
+  SNAKEFILE=Snakefile
+else
+  SNAKEFILE=$1
+fi
+
 conda activate "${ENV_NAME}"
 
 # the start of analysis
 SECONDS=0
 
 if [[ ${USETASKSET} -eq 1 ]]; then
-  echo "Using ${STARTCPU} to ${ENDCPU} cpus..." >"${LOGFILE}"
+  echo "> Using ${STARTCPU} to ${ENDCPU} cpus..." >>"${LOGFILE}"
   ((CORENUM = ENDCPU - STARTCPU + 1))
-  nohup taskset -c ${STARTCPU}-${ENDCPU} snakemake -s "$1" --use-conda --resources mem_mb="${MEMORY}" --cores "${CORENUM}" "${ATTACHMENTS}" &>"${LOGFILE}" &
+
+  taskset -c ${STARTCPU}-${ENDCPU} nohup snakemake -s "${SNAKEFILE}" --use-conda --resources mem_mb="${MEMORY}" --rerun-trigger mtime --cores "${CORENUM}" "${ATTACHMENTS}" &>"${LOGFILE}" &
 elif [[ ${USETASKSET} -eq 0 ]]; then
-  nohup snakemake -s "$1" --use-conda --resources mem_mb="${MEMORY}" --cores "${CORENUM}" "${ATTACHMENTS}" &>"${LOGFILE}" &
+  nohup snakemake -s "${SNAKEFILE}" --use-conda --resources mem_mb="${MEMORY}" --rerun-trigger mtime --cores "${CORENUM}" "${ATTACHMENTS}" &>"${LOGFILE}" &
 else
-  echo "Set USETASKSET 1 or 0 in order to use taskset or not!"
+  echo "> Set USETASKSET 1 or 0 in order to use taskset or not!"
 fi
 
 # the end of analysis
 DURATION=$SECONDS
 
-echo "Time elapsed: $((DURATION / 3600)) hours, $(((DURATION / 60) % 60)) minutes and $((DURATION % 60)) seconds" >>"${LOGFILE}"
+echo "> Time elapsed: $((DURATION / 3600)) hours, $(((DURATION / 60) % 60)) minutes and $((DURATION % 60)) seconds" >>"${LOGFILE}"
 
 conda deactivate
